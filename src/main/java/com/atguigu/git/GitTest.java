4bdd95966c06c2adb85dd3f513a66a891904f28a
Wuqisen <wuqisen86@gmail.com>
2024-10-26 15:09:19 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.atguigu.git
+ * description:
+ *
+ * @Author wuqisen
+ * @Create 2024/10/26 下午 03:05
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
